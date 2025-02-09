e685da038e0885c0e76cfeb49112f97f5d698007
Jay-r Albano <jayrcodes@outlook.ph>
2025-02-09 15:51:49 +0800

    refactor slack

---
+++ utils/slack.py
@@ -0,0 +1,25 @@
+import requests
+from utils.config import SLACK_API_ENDPOINT, SLACK_API_TOKEN, SLACK_COOKIE
+
+def conversation_replies(channel_id, ts):
+    url = f"{SLACK_API_ENDPOINT}/api/conversations.replies"
+
+    params = {
+        "pretty": 1,
+        "channel": channel_id,
+        "ts": ts,
+    }
+
+    headers = {
+        "Authorization": f"Bearer {SLACK_API_TOKEN}",
+        "Cookie": SLACK_COOKIE,
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    body = {
+        "token": SLACK_API_TOKEN,
+    }
+
+    response = requests.post(url, json=body, headers=headers, params=params)
+
+    return response.json()
