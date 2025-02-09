151c670f431fbb9da87061a751a45bf721d2dd47
Jay-r Albano <jayrcodes@outlook.ph>
2025-02-09 15:51:22 +0800

    refactor: test slack 03

--- test_slack_03_thread_replies.py
+++ test_slack_03_thread_replies.py
@@ -1,39 +1,18 @@
-import requests
-import sys
 import json
 import pydash
 import re
-from utils.config import SLACK_API_ENDPOINT, SLACK_API_TOKEN, SLACK_COOKIE
+from utils.error import try_with_ai
+from utils.slack import conversation_replies
 
+def get_thread_messages():
+    def get_slack_curl(): return open('data/slack_curl.txt', 'r').read()
+    slack_curl = try_with_ai(get_slack_curl)
 
-def conversation_replies(channel_id, ts):
-    url = f"{SLACK_API_ENDPOINT}/api/conversations.replies"
+    def get_channel_id(): return re.search(r'\"channel\"\\r\\n\\r\\n([A-Z0-9]+)', slack_curl).group(1)  
+    channel_id = try_with_ai(get_channel_id)
 
-    params = {
-        "pretty": 1,
-        "channel": channel_id,
-        "ts": ts,
-    }
-
-    headers = {
-        "Authorization": f"Bearer {SLACK_API_TOKEN}",
-        "Cookie": SLACK_COOKIE,
-        "Content-Type": "application/json; charset=utf-8",
-    }
-
-    body = {
-        "token": SLACK_API_TOKEN,
-    }
-
-    response = requests.post(url, json=body, headers=headers, params=params)
-
-    return response.json()
-
-def main():
-    slack_curl = open('data/slack_curl.txt', 'r').read()
-
-    channel_id = re.search(r'\"channel\"\\r\\n\\r\\n([A-Z0-9]+)', slack_curl).group(1)
-    ts = re.search(r'\"ts\"\\r\\n\\r\\n([0-9.]+)', slack_curl).group(1)
+    def get_ts(): return re.search(r'\"ts\"\\r\\n\\r\\n([0-9.]+)', slack_curl).group(1)
+    ts = try_with_ai(get_ts)
 
     # channel_id = pydash.get(sys.argv, 1)
     # if not channel_id:
@@ -43,27 +22,27 @@ def main():
     # if not ts:
     #     raise ValueError("Thread timestamp is required")
 
-    data = conversation_replies(channel_id=channel_id, ts=ts)
+    data = try_with_ai(lambda: conversation_replies(channel_id=channel_id, ts=ts))
 
-    users = json.load(open('data/slack_users.json', 'r'))
+    def get_users(): return json.load(open('data/slack_users.json', 'r'))       
+    users = try_with_ai(get_users)
 
-    # clear data/slack_thread_messages.txt
-    with open('data/slack_thread_messages.txt', 'w') as file:
-        file.write('')
+    def write_thread_messages():
+        with open('data/slack_thread_messages.txt', 'w') as file:
+            file.write('')
+    try_with_ai(write_thread_messages)
 
-    for message in data['messages']:
-        # find by array
-        name = next((user for user in users if user['id'] == message['user']), None)
-        name = pydash.get(name, 'real_name') or message['user']
+    def write_thread_messages():
+        for message in data['messages']:
+            name = next((user for user in users if user['id'] == message['user']), None)
+            name = pydash.get(name, 'real_name') or message['user']
 
-        # print(name + ":")
-        # print(message['text'] + "\n")
+            with open('data/slack_thread_messages.txt', 'a') as file:
+                file.write(name + ":")
+                file.write(message['text'] + "\n")
 
-        # write to data/slack_thread_messages.txt
-        with open('data/slack_thread_messages.txt', 'a') as file:
-            file.write(name + ":")
-            file.write(message['text'] + "\n")
+    try_with_ai(write_thread_messages)
 
     print("Done. Check data/slack_thread_messages.txt")
 
-main()
+get_thread_messages()
